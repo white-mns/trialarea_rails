43edc8a277351e7d3402c2c5cf8df6c4bb3a1fbb
shiro <shiro.mns@gmail.com>
2020-11-04 22:03:55 +0900

    [追加]placeholderの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
     #@latest_generate = Name.where(result_no: @latest_result).maximum("generate_no")
     #@uploaded_result = UploadedCheck.maximum("result_no")
   end
+
+  def placeholder_set
+    @placeholder = {}
+    @placeholder["Number"] = "例）1~10/50~100"
+    @placeholder["Text"]   = "例）武器/\"防具\""
+    @placeholder["Name"]   = "例）太郎/\"次郎\""
+    @placeholder["Skill"]  = "例）スラッシュ/\"ラッシュ\""
+    @placeholder["Item"]   = "例）武器/\"防具\""
+  end
 end
