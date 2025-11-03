f55dee8d0db8b82b3f2a56f3e4fcf0e03fea84f9
shiro <shiro.mns@gmail.com>
2025-11-03 22:37:12 +0900

    [変更]リンク切れ対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,9 +29,7 @@ module ApplicationHelper
     player_id_text = " [PL:" + sprintf("%d", pc_name.player_id) + "]"
 
     text = (pc_name) ? pc_name.name.html_safe + player_id_text : player_id_text
-    haml_tag :a, href: "https://gameokiba.com/trialarea/result_charalist/" + result_no_text + "/" + round_no_text + "#"+ link_no_text, target: "_blank" do
-      haml_concat text
-    end
+    haml_concat text
   end
 
   def player_name_text(player_id, pc_name)
@@ -50,7 +48,7 @@ module ApplicationHelper
     round_no_text = sprintf("%d", round_no)
     link_no_text = sprintf("%d", link_no)
 
-    link_to " キャラクター", "https://gameokiba.com/trialarea/result_charalist/" + result_no_text + "/" + round_no_text + "#"+ link_no_text, :target => "_blank"
+    return ""
   end
 
   def battle_link(battle_no, page_type)
@@ -61,7 +59,7 @@ module ApplicationHelper
 
     link_text = (page_type == 1) ? "動的" : (page_type == 2) ? "フラット" : (page_type == 3) ? "コメント付" : "";
 
-    link_to link_text, "https://gameokiba.com/trialarea/battle/" + battle_no_text + "/" + page_type_text, :target => "_blank"
+    return ""
   end
 
 
